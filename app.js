7be018ef2c22e2e881562854e8ebb7bf1176c05b
richu <richursa@gmail.com>
2019-02-26 18:00:02 +0530

    made changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 let app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.listen(3000,()=>{
     console.log("server ruunning on port 3000")
 });
 app.get("/url",(req,res,next)=>{
-    res.json(["richu","sachu","tittu","raghu"]);
+    res.end("sukhamanode");
+});
+app.post("/key",(req,res)=>{
+    var key = req.body.key
+    console.log(key)
 });
